f3e11aff205bb9659603bb70e290ee0c6a860cbf
marblekloud <kelvinhokwokhei@gmail.com>
2021-08-24 15:31:27 +0800

    message

--- hoheywebsite/server/config.js
+++ hoheywebsite/server/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://b00bies69:HHnSGNoUG7RN65CT@hohey.lgmt6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    mongoURI: 'insert mongo URI',
 };
